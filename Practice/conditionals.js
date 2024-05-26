b6a370c37b4607b10b8639c0034cc5139800448f
abeedev17 <as.asuperstar05@gmail.com>
2024-05-26 16:19:59 -0400

    else condition

--- Practice/conditionals.js
+++ Practice/conditionals.js
@@ -2,9 +2,27 @@
 let num = 3;
 if (num > 0) {
   console.log(`${num} is a positive number`);
+} else {
+  console.log(`${num} is a negative number.`);
+}
+
+num = -3;
+if (num > 0) {
+  console.log(`${num} is a positive number.`);
+} else {
+  console.log(`${num} is a negative number.`);
 }
 
 let isRaining = true;
 if (isRaining) {
-  console.log("Remember to take your rain coat.");
+  console.log("You need a rain coat.");
+} else {
+  console.log("No need for a rain cost.");
+}
+
+isRaining = false;
+if (isRaining) {
+  console.log("You need a rain coat.");
+} else {
+  console.log("No need for a rain coat.");
 }
