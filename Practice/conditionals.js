ad87d4507526b8d19f1c9c684124d6da700d574b
abeedev17 <as.asuperstar05@gmail.com>
2024-05-26 16:33:30 -0400

    switch case

--- Practice/conditionals.js
+++ Practice/conditionals.js
@@ -48,3 +48,63 @@ if (weather === "rainy") {
 } else {
   console.log("No need for rain coat.");
 }
+
+// Switch
+let weather1 = "cloudy";
+switch (weather1) {
+  case "rainy":
+    console.log("You need a rain coat.");
+    break;
+  case "cloudy":
+    console.log("It might be cold, you need a jacket.");
+    break;
+  case "sunny":
+    console.log("Go out freely");
+    break;
+  default:
+    console.log("No need for rain coat.");
+}
+
+let dayUserInput = prompt("What day is today?");
+let day = dayUserInput.toLowerCase();
+
+switch (day) {
+  case "monday":
+    console.log("Today is Monday.");
+    break;
+  case "tuesday":
+    console.log("Today is Tuesday.");
+    break;
+  case "wednesday":
+    console.log("Today is Wednesday.");
+    break;
+  case "thursday":
+    console.log("Today is Thursday.");
+    break;
+  case "friday":
+    console.log("Today is Friday");
+    break;
+  case "saturday":
+    console.log("Today is Saturday");
+    break;
+  case "sunday":
+    console.log("Today is Sunday");
+    break;
+  default:
+    console.log("It is not a week day");
+}
+
+let num1 = prompt("Enter number");
+switch (true) {
+  case num1 > 0:
+    console.log("Number is positive");
+    break;
+  case num1 === 0:
+    console.log("Number is zero");
+    break;
+  case num1 < 0:
+    console.log("Number is negative");
+    break;
+  default:
+    console.log("Entered value was not a number");
+}
