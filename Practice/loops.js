eebef917a7344bc77aab1e4d22b4c84bde82b4f6
abeedev17 <as.asuperstar05@gmail.com>
2024-05-26 16:53:32 -0400

    loop for

---
+++ Practice/loops.js
@@ -0,0 +1,45 @@
+// For
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+}
+
+let sum = 0;
+for (let i = 0; i < 101; i++) {
+  sum += i;
+}
+
+console.log(sum);
+
+let sum1 = 0;
+for (let i = 0; i < 101; i += 2) {
+  sum1 += i;
+}
+
+console.log(sum1);
+
+let total = 0;
+for (let i = 0; i < 101; i++) {
+  if (i % 2 == 0) {
+    total += i;
+  }
+}
+
+console.log(total);
+
+const nums = [1, 2, 3, 4, 5];
+for (let i = 0; i < 6; i++) {
+  console.log(nums[i]);
+}
+
+for (let i = 5; i >= 0; i--) {
+  console.log(i);
+}
+
+const nums1 = [1, 2, 3, 4, 5];
+const lastIndex = nums.length - 1;
+const newArray = [];
+for (let i = lastIndex; i >= 0; i--) {
+  newArray.push(nums[i]);
+}
+
+console.log(newArray);
