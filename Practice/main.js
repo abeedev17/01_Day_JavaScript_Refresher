dd1a5c54b96175409c1bd48e80ae86f29e6f90e2
abeedev17 <as.asuperstar05@gmail.com>
2024-05-26 13:40:13 -0400

    Arrays: using array constructor

--- Practice/main.js
+++ Practice/main.js
@@ -3,3 +3,10 @@ let firstName = "James";
 firstName = "Adam";
 
 const PI = 3.14;
+// Arrays
+// Using Array constructor
+// syntax
+const arr1 = Array();
+// or
+// let arr = new Array();
+console.log(arr1);
