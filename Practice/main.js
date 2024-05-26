32071d8d4db17b8ca435b60584ab05321f2ba2f9
abeedev17 <as.asuperstar05@gmail.com>
2024-05-26 13:47:33 -0400

    Array can have items of different data types

--- Practice/main.js
+++ Practice/main.js
@@ -51,3 +51,20 @@ const arr3 = [
   { skills: ["HTML", "CSS", "JS", "React", "Python"] },
 ]; //arr3 containing different data types
 console.log(arr3);
+
+// Creating an array using split
+let js = "JavaScript";
+const charsInJavaScript = js.split("");
+
+console.log(charsInJavaScript);
+
+let companiesString = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+const companies = companiesString.split(",");
+
+console.log(companies);
+
+let txt =
+  "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+const words = txt.split(" ");
+
+console.log(words);
