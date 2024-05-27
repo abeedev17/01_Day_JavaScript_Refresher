ab7306368dd048316071e0f20f9109b1b03eee83
abeedev17 <as.asuperstar05@gmail.com>
2024-05-27 05:40:17 -0400

    Function Declaration: Function without a parameter and return

---
+++ Practice/functions.js
@@ -0,0 +1,19 @@
+// Function Declaration
+// Function without a parameter and return
+function square() {
+  let num = 2;
+  let sq = num * num;
+  console.log(sq);
+}
+
+square();
+
+function addTwoNumbers() {
+  let numOne = 10;
+  let numTwo = 20;
+  let sum = numOne + numTwo;
+
+  console.log(sum);
+}
+
+addTwoNumbers();
