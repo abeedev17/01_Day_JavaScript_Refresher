7058888e448b3bca9021a0451a33c482ae07504d
abeedev17 <as.asuperstar05@gmail.com>
2024-05-27 00:32:39 -0400

    loop for of

--- Practice/loops.js
+++ Practice/loops.js
@@ -63,3 +63,14 @@ do {
   console.log(count2);
   count2++;
 } while (count2 < 11);
+
+// for of
+const numbers = [1, 2, 3, 4, 5];
+for (const number of numbers) {
+  console.log(number);
+}
+
+const countries = ["Finland", "Sweden", "Norway", "Denmark", "Iceland"];
+for (const country of countries) {
+  console.log(country.toUpperCase());
+}
