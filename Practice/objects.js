68cd349ca679d15efab1c145ca740a8a7403a368
abeedev17 <as.asuperstar05@gmail.com>
2024-05-27 04:30:11 -0400

    Object Methods: Getting object keys using Object.keys()

--- Practice/objects.js
+++ Practice/objects.js
@@ -41,3 +41,32 @@ person1.getPersonInfo = function () {
 
 console.log(person1);
 console.log(person1.getPersonInfo());
+
+// Object Methods
+
+const person2 = {
+  firstName: "James",
+  age: 54,
+  country: "England",
+  city: "London",
+  skills: ["HTML", "CSS", "JavaScript"],
+  title: "teacher",
+  address: {
+    street: "Heitamienkatu 16",
+    pobox: 2002,
+    city: "London",
+  },
+  getPersonInfo1: function () {
+    return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`;
+  },
+};
+
+const copyPerson = Object.assign({}, person2);
+console.log(copyPerson);
+
+// Getting object keys using Object.keys()
+// Object.keys: To get the keys or properties of an object as an array
+const keys = Object.keys(copyPerson);
+console.log(keys);
+const address = Object.keys(copyPerson.address);
+console.log(address);
