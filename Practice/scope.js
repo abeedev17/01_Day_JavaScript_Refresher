e171348bf1754770b24da2b2c5e7732a1bb87964
abeedev17 <as.asuperstar05@gmail.com>
2024-05-27 04:00:47 -0400

    Global Scopes

--- Practice/scope.js
+++ Practice/scope.js
@@ -10,3 +10,21 @@ function letsLearnScope() {
 }
 
 console.log(a, b);
+
+// Global Scope
+
+let a1 = "JavaScript";
+let b1 = 10;
+
+function letsLearnScope1() {
+  console.log(a1, b1);
+  if (true) {
+    let a1 = "Python";
+    let b1 = 100;
+    console.log(a1, b1);
+  }
+  console.log(a1, b1);
+}
+
+letsLearnScope1();
+console.log(a1, b1);
