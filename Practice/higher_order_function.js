8d909aac6b04223953dd00e4b66cca0cfc9578a9
abeedev17 <as.asuperstar05@gmail.com>
2024-05-28 18:17:39 -0400

    Callback returning function

--- Practice/higher_order_function.js
+++ Practice/higher_order_function.js
@@ -8,3 +8,39 @@ function cube(Callback, n) {
 }
 
 console.log(cube(Callback, 3));
+
+// Returning function
+const higherOrder = (n) => {
+  const doSomething = (m) => {
+    const doWhatEver = (t) => {
+      return 2 * n + 3 * m + t;
+    };
+    return doWhatEver;
+  };
+  return doSomething;
+};
+
+console.log(higherOrder(2)(3)(10));
+
+const numbers = [1, 2, 3, 4, 5];
+const sumArray = (arr) => {
+  let sum = 0;
+  const callback = function (element) {
+    sum += element;
+  };
+  arr.forEach(callback);
+  return sum;
+};
+
+console.log(sumArray(numbers));
+
+const sumArray1 = (arr) => {
+  let sum = 0;
+  arr.forEach(function (element) {
+    sum += element;
+  });
+
+  return sum;
+};
+
+console.log(sumArray1(numbers));
