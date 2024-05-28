abf70066d91e03d903a33122ce69fd60b3bcfc8c
abeedev17 <as.asuperstar05@gmail.com>
2024-05-28 18:28:50 -0400

    Destructuring: Accessing array items using a loop

---
+++ Practice/destructuring_and_spreading.js
@@ -0,0 +1,12 @@
+// Destructuring arrays
+// Accessing array items using a loop
+const numbers = [1, 2, 3];
+const countries = ["Finland", "Sweden", "Norway"];
+
+for (const number of numbers) {
+  console.log(number);
+}
+
+for (const country of countries) {
+  console.log(country);
+}
