b2dddebfedcf52e12ccc8d3774cc4f4c8d90e197
abeedev17 <as.asuperstar05@gmail.com>
2024-05-28 18:37:55 -0400

    Accessing array items manually

--- Practice/destructuring_and_spreading.js
+++ Practice/destructuring_and_spreading.js
@@ -10,3 +10,18 @@ for (const number of numbers) {
 for (const country of countries) {
   console.log(country);
 }
+
+// Accessing array items manually
+const numbers1 = [1, 2, 3];
+let num1 = numbers1[0];
+let num2 = numbers1[1];
+let num3 = numbers1[2];
+
+console.log(num1, num2, num3);
+
+const countries1 = ["Finland", "Sweden", "Norway"];
+let fin = countries1[0];
+let swe = countries1[1];
+let nor = countries1[2];
+
+console.log(fin, swe, nor);
