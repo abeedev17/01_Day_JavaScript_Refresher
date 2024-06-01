dee977c9e06e95e7d751d2e83c67058c93c4d08f
abeedev17 <as.asuperstar05@gmail.com>
2024-06-01 18:51:30 -0400

    Spread operator to copy array

--- Practice/spread_or_rest_operator.js
+++ Practice/spread_or_rest_operator.js
@@ -21,3 +21,22 @@ let [gem, fra, ...nordicCountries] = countries;
 console.log(gem);
 console.log(fra);
 console.log(nordicCountries);
+
+// Spread operator to copy array
+const evens = [0, 2, 4, 6, 8, 10];
+const evenNumbers = [...evens];
+
+const odds = [1, 3, 5, 7, 9];
+const oddNumbers = [...odds];
+
+const wholeNumbers = [...evens, ...odds];
+
+console.log(evenNumbers);
+console.log(oddNumbers);
+console.log(wholeNumbers);
+
+const frontEnd = ["HTML", "CSS", "JS", "React"];
+const backEnd = ["Node", "Express", "MongoDB"];
+const fullStack = [...frontEnd, ...backEnd];
+
+console.log(fullStack);
