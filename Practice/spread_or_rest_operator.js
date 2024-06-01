5485f1cde83fa4f236f528ece161f7505236c8c1
abeedev17 <as.asuperstar05@gmail.com>
2024-06-01 18:47:53 -0400

    Spread operator to get the rest of array elements

---
+++ Practice/spread_or_rest_operator.js
@@ -0,0 +1,23 @@
+// Spread operator to get the rest of array elements
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let [num1, num2, num3, ...rest] = nums;
+
+console.log(num1, num2, num3);
+console.log(rest);
+
+const countries = [
+  "Germany",
+  "France",
+  "Belgium",
+  "Finland",
+  "Sweden",
+  "Norway",
+  "Denmark",
+  "Iceland",
+];
+
+let [gem, fra, ...nordicCountries] = countries;
+
+console.log(gem);
+console.log(fra);
+console.log(nordicCountries);
