60db724b600f1c92ac894bb1f98d89c62bffe89e
abeedev17 <as.asuperstar05@gmail.com>
2024-06-11 16:52:24 -0400

    map function

---
+++ Practice/map_function.js
@@ -0,0 +1,9 @@
+const countries = ["Finland", "Poland", "France", "Spain"];
+const newCountries = countries.map((country) => country.toUpperCase());
+
+console.log(newCountries);
+
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map((n) => n ** 2);
+
+console.log(squares);
