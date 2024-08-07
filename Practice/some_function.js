06d7f7f6bf2220e8d4d5b5eba1df2f698fb52897
abeedev17 <as.asuperstar05@gmail.com>
2024-08-08 01:01:26 +0300

    some function example2

--- Practice/some_function.js
+++ Practice/some_function.js
@@ -3,3 +3,10 @@ const someAreEvens = numbers.some((n) => n % 2 === 0);
 const someAreOdds = numbers.some((n) => n % 2 !== 0);
 console.log(someAreEvens);
 console.log(someAreOdds);
+
+// Example 2
+const evens = [0, 2, 4, 6, 8, 10];
+const someAreEvens1 = evens.some((n) => n % 2 === 0);
+const someAreOdds1 = evens.some((n) => n % 2 !== 0);
+console.log(someAreEvens1);
+console.log(someAreOdds1);
